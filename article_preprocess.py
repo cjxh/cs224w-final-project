ee524b08f3e3cd4adbb9076e24cd5a9c314e984d
cjxh <christothetina.h@gmail.com>
2018-12-04 11:15:45 -0800

    preprocess articles correctly

--- article_preprocess.py
+++ article_preprocess.py
@@ -1,51 +1,112 @@
-import progressbar
-import pickle
+import tqdm
+from utils import load_pickle, dump_pickle
 
 
-class ArticleTextData(object):
+'''
+filter out data not in ground truth (store in a pickle file)
+-> get base url and check if in the ground truth set
+get 1000 nodes
+get 1000 neighbors of those nodes
+get 1000 neighbors of neighbor nodes
+'''
+class ArticleData(object):
+    def __init__(self):
+        self.node_id_counter = 0
+        self.article_to_node_mapping = {}
+        self.ground_truth_sources = load_pickle("ground_truth.pickle").keys()
+    
+    def open(self, filepath):
+        self.infile  = open(filepath, 'r')
+        self.filepath = filepath
+        path = filepath.split('/')
+        filename = path[len(path)-1]
+        self.snapfile = open('data/snap-'+filename, 'w')
 
-	def __init__(self):
-		self.node_id_counter = 0
-		self.article_to_node_mapping = {}
+    def close(self):
+        print self.article_to_node_mapping
+        dump_pickle('data/article-node-id.pickle', self.article_to_node_mapping)
+        self.infile.close()
+        self.snapfile.close()
 
-	def open(self, filepath):
-		self.infile  = open(filepath, 'r')
-		path = filepath.split('/')
-		filename = path[len(path)-1]
-		self.snapfile = open('data/snap-'+filename, 'w')
-	
-	def get_node_id(self, article_url):
-		if article_url not in self.article_to_node_mapping.keys():
-			self.article_to_node_mapping[article_url] = self.node_id_counter
-			self.node_id_counter += 1
+    def is_valid_source(self, article_url):
+        article_url_split = article_url.split('/')
+        base_url = article_url_split[2] #.split('.')[0]
+        for source in self.ground_truth_sources:
+            if source in base_url:
+                return True
+        return False
 
-		return self.article_to_node_mapping[article_url]
+    def get_node_id(self, article_url):
+        if article_url not in self.article_to_node_mapping.keys():
+            self.article_to_node_mapping[article_url] = self.node_id_counter
+            self.node_id_counter += 1
 
-	def generate_node_mapping(self):
-		counter = 0
-		for line in progressbar.progressbar(self.infile):
-			data = line.decode('utf-8').strip("\n").split("\t")
+        return self.article_to_node_mapping[article_url]
 
-			source_node_id = self.get_node_id(data[0])
+    def generate_node_mapping(self):
+        counter = 0
+        first_set = set([])
+        second_set = set([])
 
-			for i in range(2, len(data)):
-				dest_node_id = self.get_node_id(data[i])
-				self.snapfile.write(str(source_node_id) + "\t" + str(dest_node_id) + "\n")
+        third_set = set([])
 
-			counter += 1
-			if counter == 10000:
-				break
+        for line in tqdm.tqdm(self.infile):
+            data = line.decode('utf-8').strip("\n").split("\t")
 
-	def dump_pickle(self, output=False):
-		with open('data/article-node-id.pickle', 'wb') as article_node_pickle:
-			pickle.dump(self.article_to_node_mapping, article_node_pickle, protocol=pickle.HIGHEST_PROTOCOL)
+            if self.is_valid_source(data[0]):
+                source_node_id = self.get_node_id(data[0])
 
-	def close(self):
-		self.dump_pickle(output=True)
-		self.infile.close()
-		self.snapfile.close()
+                for i in range(2, len(data)):
+                    if self.is_valid_source(data[i]):
+                        dest_node_id = self.get_node_id(data[i])
+                        first_set.add(dest_node_id)
+                        second_set.add(source_node_id)
+                        # if level == 1:
+                        #     first_set.add(dest_node_id)
+                        # elif level == 2:
+                        #     if source_node_id in first_set:
+                        #         second_set.add(dest_node_id)
+                        #     else:
 
-td = ArticleTextData()
+                        # elif level == 3 and source_node_id in third_set:
+                        #     third_set.add(dest_node_id)
+                        # else:
+                        #     continue
+                        self.snapfile.write(str(source_node_id) + "\t" + str(dest_node_id) + "\n")
+
+                counter += 1
+                if counter == 1000:
+                    break
+
+        print "~~~~~~~~~~~~~~~~~" + str(len(first_set))
+
+        self.open(self.filepath)
+        counter = 0
+        for line in tqdm.tqdm(self.infile):
+            data = line.decode('utf-8').strip("\n").split("\t")
+
+            if not self.is_valid_source(data[0]):
+                continue 
+
+            if data[0] in second_set:
+                source_node_id = self.get_node_id(data[0])
+
+                for i in range(2, len(data)):
+                    if self.is_valid_source(data[i]):
+                        dest_node_id = self.get_node_id(data[i])
+                        if dest_node_id in first_set:
+                            continue 
+
+                        third_set.add(source_node_id)
+                        self.snapfile.write(str(source_node_id) + "\t" + str(dest_node_id) + "\n")
+
+                    counter += 1
+                    if counter == 1000:
+                        break
+
+
+
+td = ArticleData()
 td.open('data/web-2016-09-links-clean-1.txt')
 td.generate_node_mapping()
 td.close()
---
+++ utils.py
@@ -0,0 +1,58 @@
+import cPickle as pickle
+
+
+def dump_pickle(filename, data):
+	with open(filename, 'wb') as pickle_file:
+		pickle.dump(data, pickle_file, protocol=pickle.HIGHEST_PROTOCOL)
+		print "Successfully saved data to {}.".format(filename)
+
+'''
+Returns data from filename as a python obj.
+'''
+def load_pickle(filename, output=False):
+	with open(filename, 'rb') as pickle_file:
+		data = pickle.load(pickle_file)
+		if output:
+			data
+		print "Successfully loaded data from {}.".format(filename)
+		return data
+
+def store_ground_truth():
+	ground_truth = {
+		"occupydemocrats": 0,
+		"buzzfeed" : 0.07,
+		"breitbart": 0.07,
+		"donaldjtrump": 0.1,
+		"infowars" : 0.1,
+		"yahoo" : 0.125,
+		"huffingtonpost" : 0.2,
+		"theblaze" : 0.2,
+		"fox" : 0.23,
+		"rushlimbaugh" : 0.32,
+		"abc" : 0.37,
+		"msnbc" : 0.37,
+		"drudgereport" : 0.39,
+		"nbc" : 0.43,
+		"cnn" : 0.43,
+		"cbs" : 0.5,
+		"theatlantic" : 0.62,
+		"usatoday" : 0.64,
+		"nytimes" : 0.75,
+		"kansascity" : 0.76,
+		"seattletimes" : 0.76,
+		"time" : 0.82,
+		"washingtonpost" : 0.83,
+		"denverpost" : 0.83,
+		"ap" : 0.83,
+		"politico" : 0.83,
+		"dallasnews" : 0.87,
+		"latimes" : 0.87,
+		"wsj" : 0.9,
+		"theguardian" : 0.92,
+		"pbs" : 0.92,
+		"npr" : 0.95,
+		"bbc" : 0.95,
+		"reuters" : 0.96,
+		"economist" : 1
+	}
+	dump_pickle("ground_truth.pickle", ground_truth)
